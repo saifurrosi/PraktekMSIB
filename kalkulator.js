5144c1c64a8a32234937e5f38b4b24f47273e085
Ahmad Saifur Rosi <126704309+saifurrosi@users.noreply.github.com>
2023-03-16 00:02:18 +0700

    Add files via upload

---
+++ kalkulator.js
@@ -0,0 +1,59 @@
+function hitung() {
+    //tangkap id form
+    var frm = document.getElementById('calcForm');
+    var a1 = parseFloat(frm.angka1.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var tombol = frm.tombol.value;
+
+    
+    switch (tombol) {
+        case 'btn_tambah':
+            if (isNaN(a1) || isNaN(a2)) {
+                alert("Harap masukkan angka!!");
+            }
+            else {
+                var total = a1 + a2; //rumus
+                frm.hasil.value = total; //penempatan hasil 
+            }
+            break;
+        case 'btn_kurang':
+            if (isNaN(a1) || isNaN(a2)) {
+                alert("Harap masukkan angka!!");
+            }
+            else {
+                var total = a1 - a2; //rumus
+                frm.hasil.value = total; //penempatan hasil 
+            }
+            break;
+        case 'btn_kali':
+            if (isNaN(a1) || isNaN(a2)) {
+                alert("Harap masukkan angka!!");
+            }
+            else {
+                var total = a1 * a2; //rumus
+                frm.hasil.value = total; //penempatan hasil 
+            }
+            break;
+        case 'btn_bagi':
+            if (isNaN(a1) || isNaN(a2)) {
+                alert("Harap masukkan angka!!");
+            }
+            else {
+                var total = a1 / a2; //rumus
+                frm.hasil.value = total; //penempatan hasil 
+            }
+            break;
+        case 'btn_pangkat':
+            if (isNaN(a1) || isNaN(a2)) {
+                alert("Harap masukkan angka!!");
+            }
+            else {
+                var total = Math.pow(a1, a2); //rumus
+                frm.hasil.value = total; //penempatan hasil
+            }
+            break;
+
+        default:
+            break;
+    }
+}
